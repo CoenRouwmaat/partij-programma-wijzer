eeee07895e005bc86e84ffea0d0944d41de9e4d8
CoenRouwmaat <coenrouwmaat@proton.me>
2025-09-20 12:50:05 +0200

    feat(config): improve config dataclass logic, add mistral config

--- src/config.py
+++ src/config.py
@@ -1,35 +1,44 @@
 # TODO: look into Pydantic for dataclass definitions
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Literal
 
 
 @dataclass
 class FilePaths:
-    project_root = Path.cwd()
-    data_dir = project_root / "data"
-    pdf_dir = data_dir / "pdfs"
-    markdown_dir = data_dir / "markdown_raw"
-    image_dir = data_dir / "images"
-    clean_markdown_dir = data_dir / "markdown_clean"
-    json_dir = data_dir / "json"
-    chunk_dir = data_dir / "chunks"
+    project_root: Path = Path.cwd()
+    data_dir: Path = project_root / "data"
+    pdf_dir: Path = data_dir / "pdfs"
+    markdown_dir: Path = data_dir / "markdown_raw"
+    image_dir: Path = data_dir / "images"
+    clean_markdown_dir: Path = data_dir / "markdown_clean"
+    json_dir: Path = data_dir / "json"
+    chunk_dir: Path = data_dir / "chunks"
+
+
+@dataclass
+class MistralConfig:
+    mistral_ocr_model: str = "mistral-ocr-latest"
 
 
 @dataclass
 class MarkdownHeaderTextSplitterConfig:
-    headers_to_split_on = [
-        ("#", "Hoofdstuk"),
-        ("##", "Sectie"),
-        ("###", "Subsectie"),
-    ]
-    strip_headers = True
+    headers_to_split_on: list[tuple[str, str]] = field(
+        default_factory=lambda: [
+            ("#", "Hoofdstuk"),
+            ("##", "Sectie"),
+            ("###", "Subsectie"),
+        ]
+    )
+    strip_headers: bool = True
 
 
 @dataclass
 class RecursiveCharacterTextSplitterConfig:
-    max_chunk_size = 512
-    chunk_overlap = 50
-    separators = ["\n\n", ".\n", "\n", ".", " ", ""]
+    chunk_size: int = 512
+    chunk_overlap: int = 50
+    separators: list[str] = field(
+        default_factory=lambda: ["\n\n", ".\n", "\n", ".", " ", ""]
+    )
     keep_separator: Literal["start", "end"] = "end"
