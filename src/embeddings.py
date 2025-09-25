9d60aac13a52919988d3c5831b7b22598f9b2f93
CoenRouwmaat <coenrouwmaat@proton.me>
2025-09-25 23:47:26 +0200

    feat(core): add basic generate_embeddings function

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,4 +1,7 @@
+# TODO: create gemini client config class
+
 from dotenv import load_dotenv
+from google.genai import Client
 
 from src.data_classes import PartyDocumentChunk
 
@@ -9,3 +12,11 @@ def format_embedding_content(chunk: PartyDocumentChunk) -> str:
     """Append chapter, section, subsection and content for embedding"""
     embedding_content = f"{chunk.chapter}\n{chunk.section}\n{chunk.subsection}\n{chunk.content}"
     return embedding_content
+
+
+def generate_embeddings(gemini_client: Client, embedding_content: list[str]):
+    embedding_result = gemini_client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=embedding_content
+        )
+    return embedding_result
