c7bc1c3c6dbd67c28239bb430cbc4501cb19c567
CoenRouwmaat <coenrouwmaat@proton.me>
2025-09-26 00:49:09 +0200

    feat(clients): add MistralClient setup

--- src/clients.py
+++ src/clients.py
@@ -1,12 +1,22 @@
+# TODO: add upload, process & parse_response functions to MistralClient 
+
 from dataclasses import asdict
 
+from mistralai import Mistral
 import psycopg2
 from psycopg2.extensions import connection, cursor
 
-from src.config import PostgresClientConfig
+from src.config import MistralConfig, PostgresClientConfig
 
 
-class PostgresClient():
+class MistralClient:
+    def __init__(self, config: MistralConfig):
+        self._api_key = config.api_key
+        if not self._api_key:
+            raise ValueError("Could not find MISTRAL_API_KEY in the environment variables.")
+        self.ocr_model = config.mistral_ocr_model
+        self.mistral = Mistral(api_key=self._api_key)
+
 
     def __init__(self, config: PostgresClientConfig):
         self.connection: connection = psycopg2.connect(**asdict(config))
