32669e135c508a6ab4beb6e45c69a6ac7692a829
CoenRouwmaat <coenrouwmaat@proton.me>
2025-09-28 21:46:44 +0200

    feat(enum): add DocumentStructure enum

--- src/enums.py
+++ src/enums.py
@@ -6,3 +6,9 @@ class Party(StrEnum):
     CDA = "CDA"
     PVDA = "PvdA"
     VIJFTIG_PLUS = "50PLUS"
+
+
+class DocumentStructure(StrEnum):
+    CHAPTER = "Hoofdstuk"
+    SECTION = "Sectie"
+    SUBSECTION = "Subsectie"
